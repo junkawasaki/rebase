0328f5c49474bb8b426d879a9c0d24b01d594388
yyoshida <naka_tani_tora@yahoo.co.jp>
2016-09-19 16:20:38 +0000

    Revert "YMO"
    This reverts commit f99bed1d0f9fe2020d2100bda88276b7e250f0d6.

--- chap09/list0903.c
+++ chap09/list0903.c
@@ -4,9 +4,9 @@ int main()
 {
     char str[5];
 
-    str[0] = 'Y';
-    str[1] = 'M';
-    str[2] = 'O';
+    str[0] = 'S';
+    str[1] = 'O';
+    str[2] = 'D';
     str[3] = '\n';
     
     printf("文字列strは\"%s\"です。\n", str);
