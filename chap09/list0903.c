a63743381f46ffbfbaacd31ab5fbea6324b8bf61
yyoshida <naka_tani_tora@yahoo.co.jp>
2016-09-19 14:59:46 +0000

    SPC

--- chap09/list0903.c
+++ chap09/list0903.c
@@ -4,9 +4,9 @@ int main()
 {
     char str[5];
 
-    str[0] = 'A';
-    str[1] = 'H';
-    str[2] = 'O';
+    str[0] = 'S';
+    str[1] = 'P';
+    str[2] = 'C';
     str[3] = '\n';
     
     printf("文字列strは\"%s\"です。\n", str);
